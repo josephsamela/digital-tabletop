90cd0fe70715a93f19307bc40933cf60def239bb
Joseph Samela <josephsamela@gmail.com>
2018-04-05 22:00:19 -0400

    Assets controls generated programmatically from folder contents!

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+from os import walk
 
-MAP = "static/maps/default.jpg"
+
+MAP = "static/maps/default - Default Loading Screen.jpg"
+
+MAPS_PATH = "static/maps/"
+THEME_PATH = "static/audio/theme/"
+EFFECTS_PATH = "static/audio/effect/"
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+
+    themes = generate_themes(THEME_PATH)
+    maps = generate_maps(MAPS_PATH)
+    effects = generate_effects(EFFECTS_PATH)
+
+    return render_template('index.html', map=MAP, themes=themes, maps=maps, effects=effects)
 
 @app.route("/test")
 def test():
@@ -35,14 +46,56 @@ def update():
     if request.method == "GET":
         return jsonify(map=MAP)
 
-
 @app.route("/board", methods = ['POST', 'GET'])
 def board():
-
     if request.method == "POST":
         print("post!")
+    return render_template('board.html', map=MAP)
 
-    return render_template('board.html')
+def get_file_list(path):
+    f = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        f.extend(filenames)
+    return f
+
+def generate_themes(theme_path):
+    text = ''
+    path_list = get_file_list(theme_path)
+    map_num = 1
+    for f in path_list:
+        print(f)
+        path = theme_path+f
+        title, subtitle = f.split("-")
+        template = '<button type="button" id="player'+str(map_num)+'_button" class="list-group-item list-group-item-action rounded custom_card"><div class="d-flex w-100 justify-content-between"><h6>'+title+'<small>'+subtitle+'</small></h6></div><audio controls loop preload="auto" controlsList="nodownload" style="width:100%" id="player'+str(map_num)+'"><source id="theme" src="'+path+'"></audio></button>'
+        text = text+template+'\n\n'
+        map_num += 1
+    return text
+
+def generate_effects(theme_path):
+    text = ''
+    path_list = get_file_list(theme_path)
+    map_num = 100
+    for f in path_list:
+        print(f)
+        path = theme_path+f
+        title, subtitle = f.split("-")
+        template = '<button type="button" id="player'+str(map_num)+'_button" class="list-group-item list-group-item-action rounded custom_card"><div class="d-flex w-100 justify-content-between"><h6>'+title+'<small>'+subtitle+'</small></h6></div><audio controls preload="auto" controlsList="nodownload" style="width:100%" id="player'+str(map_num)+'"><source id="theme" src="'+path+'"></audio></button>'
+        text = text+template+'\n\n'
+        map_num += 1
+    return text
+
+def generate_maps(theme_path):
+    text = ''
+    path_list = get_file_list(theme_path)
+    map_num = 1
+    for f in path_list:
+        print(f)
+        path = theme_path+f
+        title, subtitle = f.split("-")
+        template = '<button type="button" id="map'+str(map_num)+'" name="map-thumbnail" class="list-group-item list-group-item-action p-0 rounded custom_card" onclick="switchMap(this, \''+path+'\');"><div class="d-flex justify-content-between"><div class="p-2 m-0 map_description"><h5>'+title+'</h5><small>'+subtitle+'</small></div><div class="card-img-bottom map-thumbnail-'+str(map_num)+' rounded-right" style="background: url(\''+path+'\') center no-repeat;"></div></div></button>'
+        text = text+template+'\n\n'
+        map_num += 1
+    return text
 
 # curl -POST localhost:5000/board -d "static/img/map.jpg"
 
