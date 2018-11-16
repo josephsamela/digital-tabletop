a61dff98eaa190358e8c1cccff3ac71e63179093
Joseph Samela <josephsamela@gmail.com>
2018-11-16 18:58:15 -0500

    improvements to fog + light

--- js/fog.js
+++ js/fog.js
@@ -8,23 +8,28 @@ function InitThis() {
     ctx.canvas.width = $('#map-preview').width() + 1;
     ctx.canvas.height = $('#map-preview').height() + 1;
 
+    // mouse event
     $('#canvas_fog').mousedown(function (e) {
         mousePressed = true;
         Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
-
     $('#canvas_fog').mousemove(function (e) {
         if (mousePressed) {
             Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
         }
     });
-
     $('#canvas_fog').mouseup(function (e) {
         mousePressed = false;
     });
     $('#canvas_fog').mouseleave(function (e) {
         mousePressed = false;
     });
+
+    var el = document.getElementsByTagName("canvas")[1];;
+    el.addEventListener("touchstart", handleStart, false);
+    el.addEventListener("touchend", handleEnd, false);
+    el.addEventListener("touchcancel", handleCancel, false);
+    el.addEventListener("touchmove", handleMove, false);
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -78,6 +83,23 @@ function updatefogview() {
 function fogbrushsize(value) {
     fog_line_width = value
 }
+
+function handleStart(e) {
+    mousePressed = true;
+    Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+}
+function handleMove(e) {
+    if (mousePressed) {
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+    }
+}
+function handleEnd(e) {
+    mousePressed = false;
+}
+function handleCancel(e) {
+    mousePressed = false;
+}
+
 
 InitThis()
 $('#canvas_fog')[0].style.display = "difference";
--- js/light.js
+++ js/light.js
@@ -1,6 +1,10 @@
+
 function darknessSlider(c) {
     $('#darkness').val(c.value)
     $('#canvas_darkness').css('opacity', c.value);
     $('#canvas_darkness').css('background-color', 'black');
-}
 
+    ctx = $("#canvas_darkness")[0].getContext('2d');
+    ctx.canvas.width = $('#map-preview').width() + 1;
+    ctx.canvas.height = $('#map-preview').height() + 1;
+}
